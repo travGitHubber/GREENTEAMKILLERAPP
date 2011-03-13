69390e114959824e6084579007d3355c47c9aab3
Joel Buehler <joelbuehler@gmail.com>
2011-03-12 19:14:51 -0600

    Added information to get headers from a csv file, make/show a 2d array of current database, collect informationn about the database like Table names and column names in each table. Added the ability to search for a header in the database and find where it is located. Made a function to start the searching for the headers in the database.
    Need to add the following:
    Cross referencing of CSV headers to Database table Columns.
    Making of mysql insert statements to Database using the information collected
    from the findColumn information.
    Finding a way to double check if information is already stored in database.
    Finding a way to update information if needed when patient is found in database.
    Last but not least insert the statements into the database for storage.
    
    Joel Buehler 3/12/11

--- testing/tester2.php
+++ testing/tester2.php
@@ -1,42 +1,171 @@
 <?php
 $databasehost = "173.201.88.100";
 $databasename = "mis1105402314808";
-$databasetable = "testing2";
 $databaseusername ="mis1105402314808";
 $databasepassword = "G1980berlin";
 $fieldseparator = ",";
 $lineseparator = "\n";
 $csvfile = "CleanedRAWCSV.csv";
-$linearray = array();
+$fieldsArray = array();
+$tablesArray = array();
+$headersArray = array();
+$databaseArray = array(array());
+$howManytables = 0; 
+
+// echo $csvcontent;
+getHeaders($csvfile);
+getDatabaseTables($databasehost, $databaseusername, $databasepassword, $databasename);
+setupDatabasearray($databasehost, $databaseusername, $databasepassword, $databasename);
+echo count($tablesArray). " Tables in this Database <br><br>";
+// show2dArray();
+startSearching($databaseArray,$headersArray);
+/**
+ * Gets the headers from the csv file. Used later for data matching.
+ * This pushes information into the $headersArray.
+ * @param $csvfile file This is the csv file we are using.
+ */
+function getHeaders($csvfile)
+{
+	global $headersArray, $lineseparator, $fieldseparator;
+	if(!file_exists($csvfile)) { echo "File not found. Make sure you specified the correct path.\n";	exit;}
+	$file = fopen($csvfile,"r");
+	if(!$file) { echo "Error opening data file.\n"; exit; }
+	$size = filesize($csvfile);
+	if(!$size) {echo "File is empty.\n"; exit;}
+	$csvcontent = fread($file,$size);
+	fclose($file);
+	$lines = 0;
+	$linearray = array();
+
+	foreach(split($lineseparator,$csvcontent) as $line) 
+	{
+		$lines++;
+		if ($lines == 1)
+		{
+		$line = trim($line," \t");
+		$line = str_replace("\r","",$line);
+		$line = str_replace("'","\'",$line);
+		$linearray = explode($fieldseparator,$line);
+		for($i =0; $i < count($linearray); $i++){ array_push($headersArray,$linearray[$i]); echo 'Header'.$i. ' is                 ' .$linearray[$i]. '<br>'; }
+		} 
+	}
+}
+/**
+ * This is used to show all tables and their field information from the
+ * Database that is stored in a 2D Array.
+ */
+function show2dArray()
+{
+	global $databaseArray, $howManytables;
+	for($row = 0; $row < $howManytables; $row++)
+	{
+		echo 'Table is ' . $databaseArray[$row][0] . '<br>';
+		echo 'Columns in this table are <br>';
+		for($column = 1; $column < count($databaseArray[$row]); $column++)
+		{
+			if($column == 1) echo $databaseArray[$row][$column]; 
+			else echo ', '. $databaseArray[$row][$column]; 
+			
+		}
+	echo '<br><br>';
+	}	
+}
+/**
+ * Shows all the information from the database.
+ * @param $databasehost	String		Databe host address
+ * @param $databaseusername	String	Database user name
+ * @param $databasepassword	String	Database user password
+ * @param $databasename	String		Database name
+ * @param $databasetable String		Table name in Database
+ */
+function setupDatabasearray($databasehost, $databaseusername, $databasepassword, $databasename)
+{
+	global $tablesArray, $fieldsArray, $databaseArray;
+	for ($j = 0; $j < count($tablesArray); $j++)
+	{
+	$table = $tablesArray[$j];
+	getColumnNameInformation($databasehost, $databaseusername, $databasepassword, $databasename, $table);
+//	echo '<br>' .$table . ' table <br>';
+//	echo count($fieldsArray). " fieldsArray <br>";
+		for($i = 1; $i <= count($fieldsArray); $i++ )
+		{
+			$databaseArray[$j][0] = $table;
+			$databaseArray[$j][$i] = $fieldsArray[$i-1];
+		}
+//	foreach ($fieldsArray as $value) echo $value." ";
+	}
+}
+/**
+ * Collects all the Table names of a database and puts them in the Tables Array
+ * @param $databasehost	String		Databe host address
+ * @param $databaseusername	String	Database user name
+ * @param $databasepassword	String	Database user password
+ * @param $databasename	String		Database name
+ * @param $databasetable String		Table name in Database
+ */
+function getDatabaseTables($databasehost, $databaseusername, $databasepassword, $databasename)
+{
+global $tablesArray, $howManytables;
 $conn = mysql_connect($databasehost, $databaseusername, $databasepassword);
-if (!$conn) {
-    die('Could not connect: ' . mysql_error());
+$res = mysql_query("SHOW TABLES FROM $databasename");
+while($row = mysql_fetch_array($res, MYSQL_NUM)) array_push($tablesArray,$row[0]);
+$howManytables = count($tablesArray);
 }
+/**
+ * Collects all the column names of a Table and puts them in the Fields Array
+ * @param $databasehost	String		Database host address
+ * @param $databaseusername	String	Database user name
+ * @param $databasepassword	String	Database user password
+ * @param $databasename	String		Database name
+ * @param $databasetable String		Table name in Database
+ */
+function getColumnNameInformation($databasehost, $databaseusername, $databasepassword,$databasename,$databasetable)
+{
+global $fieldsArray;
+$fieldsArray = array();
+
+$conn = mysql_connect($databasehost, $databaseusername, $databasepassword);
+if (!$conn) die('Could not connect: ' . mysql_error());
 mysql_select_db($databasename);
-$result = mysql_query('select * from testing3');
-if (!$result) {
-    die('Query failed: ' . mysql_error());
-}
-/* get column metadata */
-$i = 0;
-global $linearray;
-while ($i < mysql_num_fields($result)) {
-	global $linearray;
+$result = mysql_query('select * from ' . $databasetable);
+if (!$result) die('Query failed: ' . mysql_error());
+
+for($i = 0; $i < mysql_num_fields($result); $i++) 
+	{
+	global $fieldsArray;
 	$meta = mysql_fetch_field($result, $i);
-    if (!$meta) {
-        echo "No information available<br />\n";
-    }
-    array_push($linearray,$meta->name);
-    echo "<pre>
-name:         $meta->name
-table:        $meta->table
-type:         $meta->type
-</pre>";
-    $i++;
+    if (!$meta) echo "No information available<br />\n";
+    array_push($fieldsArray,$meta->name); 
+	}
 }
-#
-foreach ($linearray as $value) {
-#
-echo $value." "; }
-mysql_free_result($result);
+/**
+ * Searches the 2d Array of Database information for the given header.
+ * @param $databaseArray 2d Array  Holds the informatino of the Database
+ * @param $searchFor String This is the header that you are searching for
+ */
+function findColumn($databaseArray, $searchFor)
+{
+global $howManytables;
+	for ($j = 0; $j < $howManytables; $j++)
+		{
+			for($i = 1; $i <= count($databaseArray[$j]); $i++ )
+			{
+			if (strcmp($searchFor, $databaseArray[$j][$i]) == 0) echo $searchFor .' was found in table '
+			 . $databaseArray[$j][0] . ' column number ' . $i. '<br>';
+			}
+		}
+}
+/**
+ * Starts the searching commands for the database Array
+ * @param $databaseArray 2D Array  This is the array to use.
+ * @param $headersArray  Array	   This is the header array to use.
+ */
+function startSearching($databaseArray,$headersArray)
+{
+	for($joel = 0; $joel < count($headersArray) ; $joel++)
+	{
+		$searchME = $headersArray[$joel];
+		findColumn($databaseArray, $searchME);
+	}
+}
 ?>
