57ca4e2d91c267c5b720b699fabdbe425b33a486
Joel Buehler <joelbuehler@gmail.com>
2011-03-15 19:49:56 -0500

    Added the following to the current File: Cross referencing of CSV headers to Database table Columns. Working on adding the Following: Making of mysql insert statements to Database using the information collected from the findColumn information. Finding a way to double check if information is already stored in database. Finding a way to update information if needed when patient is found in database. Last but not least insert the statements into the database for storage. Signed-off-by: Joel Buehler <joelbuehler@gmail.com>

--- testing/tester2.php
+++ testing/tester2.php
@@ -1,4 +1,5 @@
 <?php
+//Global values in this file.
 $databasehost = "173.201.88.100";
 $databasename = "mis1105402314808";
 $databaseusername ="mis1105402314808";
@@ -9,16 +10,32 @@ $csvfile = "CleanedRAWCSV.csv";
 $fieldsArray = array();
 $tablesArray = array();
 $headersArray = array();
+$inserterArray = array(array());
 $databaseArray = array(array());
 $howManytables = 0; 
-
+$foundHeaders = 0;
+//-------------------------------------------------------------------------------------------------
+	
 // echo $csvcontent;
+/* $testingArray = array(array());
+$testingArray2 = array('joel', 'bob', 'greg');
+$testingArray3 = array( joel1, bob1, greg1);
+$testingArray4 = array('joel2', 'bob2', 'greg2');
+
+$testingArray[0] = $testingArray2;
+$testingArray[1] = $testingArray3;
+$testingArray[2] = $testingArray4;
+$linemysql = implode(",",$testingArray[1]);
+echo $linemysql . ' what the fuck dude'; 
+*/
 getHeaders($csvfile);
 getDatabaseTables($databasehost, $databaseusername, $databasepassword, $databasename);
 setupDatabasearray($databasehost, $databaseusername, $databasepassword, $databasename);
-echo count($tablesArray). " Tables in this Database <br><br>";
-// show2dArray();
+echo '<br><b>' . count($tablesArray). " Tables in this Database </b><br><br>";
+show2dArray();
+thereplacer($databasehost, $databaseusername, $databasepassword);
 startSearching($databaseArray,$headersArray);
+//insertME($csvfile);
 /**
  * Gets the headers from the csv file. Used later for data matching.
  * This pushes information into the $headersArray.
@@ -145,13 +162,16 @@ for($i = 0; $i < mysql_num_fields($result); $i++)
  */
 function findColumn($databaseArray, $searchFor)
 {
-global $howManytables;
+global $howManytables, $foundHeaders;
 	for ($j = 0; $j < $howManytables; $j++)
 		{
 			for($i = 1; $i <= count($databaseArray[$j]); $i++ )
 			{
-			if (strcmp($searchFor, $databaseArray[$j][$i]) == 0) echo $searchFor .' was found in table '
-			 . $databaseArray[$j][0] . ' column number ' . $i. '<br>';
+			if (strcmp($searchFor, $databaseArray[$j][$i]) == 0)
+			{ echo '<br>'. $searchFor .' was found in table '
+			 . $databaseArray[$j][0] . ' column number ' . $i;
+			$foundHeaders++;
+			}
 			}
 		}
 }
@@ -162,10 +182,93 @@ global $howManytables;
  */
 function startSearching($databaseArray,$headersArray)
 {
+	global $foundHeaders, $csvfile;
 	for($joel = 0; $joel < count($headersArray) ; $joel++)
 	{
 		$searchME = $headersArray[$joel];
 		findColumn($databaseArray, $searchME);
 	}
+	echo ' <br><br> We have found '. $foundHeaders. ' headers of the '. count($headersArray). ' headers
+	found in the file ' . $csvfile .'<br>';
+	echo 'This is ' .$foundHeaders / count($headersArray) * 100 . ' percent of all the 
+	headers in the ' .$csvfile . ' file where found in the database.';
 }
+/**
+ * Replaces the $headersArray values from the csv file with the Mysql database column counterparts.
+ * @param $databasehost	String		Database host address
+ * @param $databaseusername	String	Database user name
+ * @param $databasepassword	String	Database user password
+ */
+function thereplacer($databasehost, $databaseusername, $databasepassword)
+{
+	global $headersArray, $databasename;
+	$conn = mysql_connect($databasehost, $databaseusername, $databasepassword);
+	if (!$conn) die('Could not connect: ' . mysql_error());
+	mysql_select_db($databasename);
+	for($i = 0; $i < count($headersArray) ; $i++)
+	{
+		$result = mysql_query("SELECT csvHeader,tableField FROM columns WHERE csvHeader = '$headersArray[$i]'");
+		if (!$result) { echo 'Could not run query: ' . mysql_error(); exit; }
+		$row = mysql_fetch_row($result);
+		echo "<br><b> CSV Header $i is $headersArray[$i] </b>" ;
+		$headersArray[$i] = $row[1];
+		echo "<br><i> Table Header $i is $headersArray[$i] </i>" ;
+	}
+	echo '<br>';
+}
+function insertME($csvfile)
+{	
+global $headersArray,$lineseparator,$databasehost,$databaseusername,$databasepassword,$databasename,$fieldseparator, $databaseArray; 
+if(!file_exists($csvfile)) { echo "File not found. Make sure you specified the correct path.\n"; exit;}
+$file = fopen($csvfile,"r");
+
+if(!$file) { echo "Error opening data file.\n";	exit; }
+$size = filesize($csvfile);
+
+if(!$size) { echo "File is empty.\n"; exit;}
+$csvcontent = fread($file,$size);
+fclose($file);
+
+$con = @mysql_connect($databasehost,$databaseusername,$databasepassword) or die(mysql_error());
+@mysql_select_db($databasename) or die(mysql_error());
+echo 'what the hell';	
+$lines = 0;
+$queries = "";
+$linearray = array();
+foreach(split($lineseparator,$csvcontent) as $line) 
+{
+	$lines++;
+
+	$line = trim($line," \t");
+	$line = str_replace("\r","",$line);
+	/************************************
+	This line escapes the special character. remove it if entries are already escaped in the csv file
+	************************************/
+	$line = str_replace("'","\'",$line);
+	/*************************************/
+	$linearray = explode($fieldseparator,$line);
+	for($test = 0; $test < count($headersArray) && $test == 0; $test++)
+	{
+		if(strcmp($headersArray[$test], $databaseArray[5][2]) == 0 ) echo " joel joel $linearray[$test] found in ". $databaseArray[5][0] ; 
+	}
+	
+	array_push ($linearray,3,4,5,6);
+	echo $linearray[4] . '<br>';   // what cell to add
+	$linemysql = implode("','",$linearray);
+	if (strlen($linemysql) > 1 && $lines >1) 
+	{
+	$query = "insert into $databasetable values('','$linemysql');";
+	$queries .= $query . "\n";
+	@mysql_query($query);
+	}
+}
+	$outputfile = "output.sql";
+
+		@mysql_close($con);
+		$file2 = fopen($outputfile,"w");
+		fwrite($file2,$queries);
+		fclose($file2);
+	echo "Found a total of $lines records in this csv file.\n";
+}
+
 ?>
